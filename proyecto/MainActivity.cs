19bf9077791418a3056ca6e6c730b5f5e0220231
your name <you@example.com>
2018-06-11 18:52:48 -0500

    ...

---
+++ proyecto/MainActivity.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Android.App;
+using Android.Widget;
+using Android.OS;
+using Android.Support.V7.App;
+
+namespace proyecto
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        Button btnAuth;
+        EditText editTextUsername;
+        EditText editTextPassword;
+        EditText editTexttoken;
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+            btnAuth = FindViewById<Button>(Resource.Id.btnAuth);
+
+            editTextUsername = FindViewById<EditText>(Resource.Id.editTextUsername);
+            editTextPassword = FindViewById<EditText>(Resource.Id.editTextPassword);
+            editTexttoken = FindViewById<EditText>(Resource.Id.editTexttoken);
+            btnAuth.Click += DoAuthentication;
+        }
+        private void DoAuthentication(object sender, EventArgs e)
+        {
+            if (editTextUsername.Text.Equals("bichito") && editTextPassword.Text.Equals("123456") && editTexttoken.Text.Equals(12345))
+            {
+                Toast.MakeText(this, "usuario valido", ToastLength.Short).Show();
+            }else
+            {
+                Toast.MakeText(this, "usuario invalido,este telefono se destruye", ToastLength.Short).Show();
+            }
+            
+
+        }
+    }
+
+}
+    
+
